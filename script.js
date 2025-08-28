33ac1bf0023d03768afedf649675d63791efd04a
brokenrace198397843 <brokenraceminecraft@gmail.com>
2025-08-28 14:13:52 +0200

    Update script.js Tawk.to

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+/*Tawk.to*/
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/67838a0049e2fd8dfe064dfc/1ihct64ct';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+
 /* Reset und Basis-Styles */
 * {
     margin: 0;
